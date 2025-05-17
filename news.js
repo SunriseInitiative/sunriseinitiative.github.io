64a44645c42b671436c6652a6bc4eb38f9f44603
HelixInteractive <161367758+HelixInteractive@users.noreply.github.com>
2025-05-17 11:32:55 +0100

    Updated News

--- news.js
+++ news.js
@@ -2,7 +2,7 @@ function UpdateNewsPreview(news) {
     const newsPreview = document.getElementsByClassName("breakingNewsPreview")[0];
     newsPreview.innerHTML = `
         <h2>${news.title}</h2>
-        <p>${news.content}</p>
+        ${news.content}
         <p><strong>Published on:</strong> ${news.date}</p>
     `;
 }
@@ -10,7 +10,8 @@ function UpdateNewsPreview(news) {
 function UpdateNews() {
     const newsPreview = {
         title: "Breaking News: Nintendo Switch 2 EULA drama",
-        content: "Nintendo Switch 2 EULA drama has taken the gaming world by storm. Players will no longer own \ntheir consoles, only a license to use them. The backlash to these changes has been \nwidespread, some calling this betrayal of Nintendo",
+        content: `<p>Nintendo Switch 2 EULA drama has taken the gaming world by storm. <b>Players will no longer own 
+        their consoles, only a license to use them.</b> The backlash to these changes has been widespread, some calling this the betrayal of Nintendo</p>`,
         date: new Date().toLocaleDateString()
     };
 
